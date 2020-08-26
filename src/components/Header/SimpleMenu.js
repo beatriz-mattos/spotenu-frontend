738c33da13cb81ac820a8c2a56f7dd120f36eb75
Beatriz Mattos <venganzakills@gmail.com>
2020-08-26 16:29:25 -0300

    resolvendo problemas no menu dropdown

--- src/components/Header/SimpleMenu.js
+++ src/components/Header/SimpleMenu.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import Reactg from 'react';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -7,10 +7,6 @@ import styled from "styled-components";
 const SubscribeButton = styled(Button)`
   background-color: green;
   height: 80px;
-`;
-
-const SubscribeMenu = styled(Menu)`
-    background-color: white;
 `
 
 const LoginButton = styled(Button)`
@@ -18,56 +14,60 @@ const LoginButton = styled(Button)`
     height: 80px;
 `
 
-const LoginMenu = styled(Menu)`
-    background-color: white;
-`
-
 export default function SimpleMenu() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+    const [anchorSubscribe, setAnchorSubscribe] = React.useState(null);
+    const [anchorLogin, setAnchorLogin] = React.useState(null);
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+    const handleClickLogin = (event) => {
+        setAnchorLogin(event.currentTarget);
+    };
+    const handleCloseLogin = () => {
+        setAnchorLogin(null);
+    };
+    const handleClickSubscribe = (event) => {
+        setAnchorSubscribe(event.currentTarget);
+    };
+    const handleCloseSubscribe = () => {
+        setAnchorSubscribe(null);
+    };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+    return (
+        <div>
+            <>
+                <SubscribeButton aria-controls="customized-menu" aria-haspopup="true" color="secondary" onClick={handleClickSubscribe}>
+                    INSCREVA-SE
+                </SubscribeButton>
+                <Menu
+                    id="simple-menu"
+                    anchorEl={anchorSubscribe}
+                    keepMounted
+                    open={Boolean(anchorSubscribe)}
+                    onClose={handleCloseSubscribe}
+                    color="primary"
+                >
+                    <MenuItem onClick={handleCloseSubscribe}>Como usuário</MenuItem>
+                    <MenuItem onClick={handleCloseSubscribe}>Como banda</MenuItem>
+                </Menu>
+            </>
 
-  return (
-    <div>
-        <>
-            <SubscribeButton aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-                INSCREVA-SE
-            </SubscribeButton>
-            <SubscribeMenu
-                id="simple-menu"
-                anchorEl={anchorEl}
-                keepMounted
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-            >
-                <MenuItem onClick={handleClose}>Como usuário</MenuItem>
-                <MenuItem onClick={handleClose}>Como banda</MenuItem>
-            </SubscribeMenu>
-        </>
+            <>
+                <LoginButton aria-controls="customized-menu" aria-haspopup="true" color="secondary" onClick={handleClickLogin}>
+                    ENTRAR
+                </LoginButton>
+                <Menu
+                    id="simple-menu"
+                    anchorEl={anchorLogin}
+                    keepMounted
+                    open={Boolean(anchorLogin)}
+                    onClose={handleCloseLogin}
+                    color="primary"
+                >
+                    <MenuItem onClick={handleCloseLogin}>Como admin</MenuItem>
+                    <MenuItem onClick={handleCloseLogin}>Como usuário</MenuItem>
+                    <MenuItem onClick={handleCloseLogin}>Como banda</MenuItem>
+                </Menu>
+            </>
+        </div>
 
-        <>
-            <LoginButton aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-                ENTRAR
-            </LoginButton>
-            <LoginMenu
-                id="simple-menu"
-                anchorEl={anchorEl}
-                keepMounted
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-            >
-                <MenuItem onClick={handleClose}>Como admin</MenuItem>
-                <MenuItem onClick={handleClose}>Como usuário</MenuItem>
-                <MenuItem onClick={handleClose}>Como banda</MenuItem>
-            </LoginMenu>
-        </>
-    </div>
-    
-  );
+    );
 }
